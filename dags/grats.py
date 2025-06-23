20c1d417745aba9dc225cecce0e1a3c8c9658685
Your Name <you@example.com>
2025-06-23 22:31:26 +0000

    nice

--- dags/grats.py
+++ dags/grats.py
@@ -6,15 +6,15 @@ def adios_world():
     print("ничего не понятно, но очень интересно")
 
 dag = DAG(
-    dag_id='four_fours',
-    description='вроде даг',
+    dag_id='GIT_ALMIGHTY_PUSH',
+    description='Женя, Володя, спасибо за контент',
     schedule_interval='@once',
     start_date=datetime(2025, 6, 10),
     catchup=False
 )
 
 bootcamp = PythonOperator(
-    task_id='offer_for_350k',  # допустимый task_id
+    task_id='offer_for_100000k',  # допустимый task_id
     python_callable=adios_world,
     dag=dag
 )
