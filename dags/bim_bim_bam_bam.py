1a803d7e8a1648823987f26081c73e6d66dc0c7a
Your Name <you@example.com>
2025-06-23 21:50:13 +0000

    fm

---
+++ dags/bim_bim_bam_bam.py
@@ -0,0 +1,22 @@
+from datetime import datetime
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+
+def adios_world():
+    print("ничего не понятно, но очень интересно")
+
+dag = DAG(
+    dag_id='four fours',
+    description='вроде даг',
+    schedule_interval='@once',
+    start_date=datetime(2025, 6, 10),
+    catchup=False
+)
+
+bootcamp = PythonOperator(
+    task_id='offer_for_350k',  # допустимый task_id
+    python_callable=hello_world,
+    dag=dag
+)
+
+bootcamp
